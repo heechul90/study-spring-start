17c82d288e0432967d05423e264a6e8bca5dc201
hclee <heechul4296@gmail.com>
2022-02-26 02:04:45 +0900

    회원서비스 개발, 테스트 개발

---
+++ src/main/java/study/spring/start/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package study.spring.start.repository;
+
+import study.spring.start.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+}
